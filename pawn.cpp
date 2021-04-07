2249bfb99d284e70fe40fc8f9ffe4361ff995ce2
Maksymilian Adamczewski <maksym48@gmail.com>
2021-04-07 15:02:21 +0200

    quick fix of onClick typo.

--- pawn.cpp
+++ pawn.cpp
@@ -13,10 +13,11 @@ Pawn :: Pawn (bool color, Coordinates initialPawnCoordinates): pawnCoordinates(i
         else        // black
         {
             body.setFillColor(sf::Color::Black);
-            body.setOutlineColor(sf::Color::White);
-            body.setOutlineThickness(0.1 * body.getRadius());
-            body.setRadius(body.getRadius() - body.getOutlineThickness()); // change radius to match white pawn in size
+             // change radius to match white pawn in size
         }
+        body.setOutlineColor(sf::Color::White);
+        body.setOutlineThickness(0.1 * body.getRadius());
+        body.setRadius(body.getRadius() - body.getOutlineThickness());
         body.setOrigin(body.getRadius(), body.getRadius()); // set origin of body to center of circle
         body.setPosition(VIEW_STEP * initialPawnCoordinates.x, VIEW_STEP * initialPawnCoordinates.y); // set body position according to pawnCoordinates
     }
